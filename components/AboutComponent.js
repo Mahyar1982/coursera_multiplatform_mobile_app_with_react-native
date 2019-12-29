6070bbec05e29ba5259fa2caf84da4f569b31fee
Mahyar1982 <mahyaar.mohammadi@gmail.com>
2019-12-29 16:14:00 +0200

    Before the assignment 2

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { Text, FlatList, ScrollView, View } from 'react-native'
 import { Card, ListItem } from 'react-native-elements'
 import { LEADERS } from '../shared/leaders'
+import { connect } from 'react-redux'
+import { baseUrl } from '../shared/baseUrl'
+import { Loading } from './LoadingComponent'
+
+const mapStateToProps = state => {
+    return {
+        leaders: state.leaders
+    }
+}
 
 const cardHistoryTitle = 'Our History'
 const cardHistoryText = 'Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.\n\The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the worlds best cuisines in a pan.'
@@ -20,13 +29,6 @@ function History() {
 }
 
 class About extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            leaders: LEADERS
-        }
-    }
     
     static navigationOptions = {
         title: 'About'
@@ -40,7 +42,7 @@ class About extends Component {
                     title={cardLeaderTitle}
                     >
                     <FlatList 
-                        data={this.state.leaders}
+                        data={this.props.leaders.leaders}
                         renderItem={LeaderShipItems}
                         keyExtractor={item => item.id.toString()}
                         />
@@ -55,22 +57,44 @@ class About extends Component {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
-                    leftAvatar={{ source: require('./images/alberto.png') }}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                     />
             )
         }
 
         const { navigate } = this.props.navigation
 
-        return(
-            <ScrollView>
-                <History />
-                <View style={{marginBottom: 30}}>
-                    <LeaderShipCard />
-                </View>
-            </ScrollView>
-        )
+        if(this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            )
+        }
+        else if(this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            )
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <View style={{marginBottom: 30}}>
+                        <LeaderShipCard />
+                    </View>
+                </ScrollView>
+            )    
+        }
     }
 }
 
-export default About
+export default connect(mapStateToProps)(About)
